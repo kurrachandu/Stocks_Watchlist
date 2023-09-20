78948f06f56b14ea5d5dc0db7f782ed3d46a0c0d
kurra chandu <nanic6465@gmail.com>
2023-09-20 18:16:21 +0530

    fetching  config data using props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Watchlist from './Watchlist';
+// import './App.css';
 
 function App() {
+  const config = [
+    { companyName: 'TCS',stockType:"BSE", stockValue: '1500.32' },
+    { companyName: 'Infosys', stockType:"NSE", stockValue: '1600.22' },
+    { companyName: 'Tech Mahindra', stockType:"BSE", stockValue: '1200.28' },
+    { companyName: 'HCL', stockType:"BSE", stockValue: '798.32' },
+    { companyName: 'Accenture', stockType:"NSE", stockValue: '232.43' },
+    { companyName: 'Bharat Petroleum', stockType:"BSE", stockValue: '390.67' },
+    { companyName: 'Axis Bank', stockType:"BSE", stockValue: '253.24' },
+    { companyName: 'ICICI Bank', stockType:"BSE" , stockValue: '100.29' },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Watchlist stocks={config} /> {/* Pass the config as props */}
+      
     </div>
   );
 }
